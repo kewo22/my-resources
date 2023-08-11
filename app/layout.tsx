6299d60ebfcd60543a3e8a5bae9cc3bf824144bc
kewin@kazix.io <kewinf271@gmail.com>
2023-08-11 15:25:59 +0530

    first commit

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,9 +1,9 @@
-import './globals.css'
+import "./globals.css";
 
 export default function RootLayout({
   children,
 }: {
-  children: React.ReactNode
+  children: React.ReactNode;
 }) {
   return (
     <html lang="en">
@@ -11,8 +11,13 @@ export default function RootLayout({
         <head /> will contain the components returned by the nearest parent
         head.tsx. Find out more at https://beta.nextjs.org/docs/api-reference/file-conventions/head
       */}
+
+      <link rel="manifest" href="/manifest.json" />
+      <link rel="apple-touch-icon" href="/icon-512x512.png"></link>
+      <meta name="theme-color" content="#000" />
+
       <head />
       <body>{children}</body>
     </html>
-  )
+  );
 }

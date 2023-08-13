b4d2946edee58206dcb4498d0cac6b4b265c4032
kewin@kazix.io <kewinf271@gmail.com>
2023-08-14 00:31:43 +0530

    style fix

--- app/page.tsx
+++ app/page.tsx
@@ -7,19 +7,19 @@ const inter = Inter({ subsets: ["latin"] });
 
 export default function Home() {
   return (
-    <main className="min-h-screen overflow-hidden flex flex-col gap-3 p-3 max-w-md m-auto bg-gradient-to-b from-indigo-500 from-10% via-sky-500 via-30% to-emerald-500 to-90%">
+    <main className="min-h-screen overflow-hidden flex flex-col gap-3 p-3 max-w-md m-auto bg-gradient-to-b from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%">
       <div className="grid grid-cols-3 gap-3">
         <textarea
           name=""
           id=""
-          className="col-span-3 bg-emerald-100 w-full outline-none border border-emerald-300 rounded-md p-3 focus:border-emerald-600 min-h-[100px]"
+          className="col-span-3 bg-emerald-100 w-full outline-none border border-emerald-600 rounded-md p-3 focus:border-emerald-950 min-h-[100px]"
           rows={3}
         ></textarea>
         <input
           type="text"
-          className="col-span-2 bg-emerald-100 w-full outline-none border border-emerald-300 rounded-md p-3 focus:border-emerald-600 "
+          className="col-span-2 bg-emerald-100 w-full outline-none border border-emerald-600 rounded-md p-3 focus:border-emerald-950"
         />
-        <button className="col-span-1 bg-emerald-100 outline-none border border-emerald-300 p-3 rounded-md">
+        <button className="select-none col-span-1 bg-emerald-300 outline-none border border-emerald-600 p-3 rounded-md">
           Save
         </button>
       </div>

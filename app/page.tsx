f8686f5129ff40f7754f6e4f077a26eafb630908
kewin@kazix.io <kewinf271@gmail.com>
2023-08-16 17:45:58 +0530

    tags api call stop temp

--- app/page.tsx
+++ app/page.tsx
@@ -4,17 +4,19 @@ import Resources from "./_components/resources";
 
 // import styles from "./page.module.css";
 
-async function getTags() {
-  const res = await fetch(`${process.env.API_URL}/tag`);
-  if (!res.ok) {
-    // This will activate the closest `error.js` Error Boundary
-    throw new Error("Failed to fetch data");
-  }
-  return res.json();
-}
+// async function getTags() {
+//   const res = await fetch(`${process.env.API_URL}/tag`);
+//   if (!res.ok) {
+//     // This will activate the closest `error.js` Error Boundary
+//     throw new Error("Failed to fetch data");
+//   }
+//   return res.json();
+// }
 
 export default async function Home() {
-  const tags = (await getTags()) as any[];
+  // const tags = (await getTags()) as any[];
+
+  const tags: any[] = [];
 
   return (
     <main className="min-h-screen overflow-hidden flex flex-col gap-3 p-3 max-w-md m-auto bg-gradient-to-b from-indigo-100 from-10% via-sky-100 via-30% to-emerald-100 to-90%">

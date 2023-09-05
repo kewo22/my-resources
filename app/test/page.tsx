d2f1c50cd5c04fa9349ffa7b0474cf4512b2c7d0
kewin@kazix.io <kewinf271@gmail.com>
2023-09-05 13:58:45 +0530

    test dep

--- app/test/page.tsx
+++ app/test/page.tsx
@@ -1,24 +1,47 @@
 "use client";
 
-import React from "react";
+import React, { useEffect, useMemo, useRef } from "react";
 import {
   useForm,
-  SubmitHandler,
   UseControllerProps,
   useController,
   FieldValues,
+  SubmitHandler,
 } from "react-hook-form";
-import { number, object, string } from "yup";
+import { boolean, number, object, string } from "yup";
 import { yupResolver } from "@hookform/resolvers/yup";
 
 interface Inputs {
   firstName: string;
   age?: number | null;
+  gender: string;
+  isActive?: number;
 }
 
 interface TextInputProps<T extends FieldValues> {
   type?: "text" | "num";
   placeHolder?: string;
+  label?: string;
+  useControllerProps: UseControllerProps<T>;
+  isDisabled?: boolean;
+}
+
+interface SelectInputs<T extends FieldValues> {
+  placeHolder?: string;
+  label?: string;
+  useControllerProps: UseControllerProps<T>;
+  selectionItems: Record<string, any>[];
+}
+
+interface CheckBoxInputs<T extends FieldValues> {
+  label?: string;
+  useControllerProps: UseControllerProps<T>;
+}
+
+interface TextInputProps<T extends FieldValues> {
+  type?: "text" | "num";
+  placeHolder?: string;
+  label?: string;
   useControllerProps: UseControllerProps<T>;
 }
 
@@ -32,11 +55,53 @@ const schema = object().shape({
     .transform((value, originalValue) =>
       String(originalValue).trim() === "" ? null : value
     ),
+  gender: string()
+    .required("Required")
+    .oneOf(["", "volvo", "saab", "mercedes", "audi"], "Invalid Selection")
+    .label("Selected Country"),
+  isActive: number()
+    .oneOf([0, 1])
+    .transform((value, originalValue) => {
+      return Boolean(originalValue) ? 1 : 0;
+    }),
 });
 
 function TextInput(props: TextInputProps<Inputs>) {
-  const { useControllerProps, placeHolder = "", type = "text" } = props;
+  const {
+    useControllerProps,
+    placeHolder = "",
+    type = "text",
+    label = "",
+    isDisabled = false,
+  } = props;
   const { field, fieldState } = useController(useControllerProps);
+
+  const className = useMemo(() => {
+    return {
+      wrapper: {
+        default:
+          "w-full border-b border-b-stone-600 flex flex-row items-center focus-within:border-blue-600 transition-all py-1 px-3",
+        error: "",
+        disabled: "",
+      },
+      label: {
+        default: "mr-1 font-semibold ",
+        error: "",
+      },
+      input: "outline-none bg-transparent flex-grow",
+      errorText: "text-red-600 text-xs font-semibold",
+    };
+  }, []);
+
+  const ref = useRef(className);
+
+  useEffect(() => {
+    if (isDisabled) {
+      const classNameCopy = JSON.parse(JSON.stringify(ref.current));
+      classNameCopy.wrapper.disabled = "!bg-gray-100";
+      ref.current = { ...classNameCopy };
+    }
+  }, [isDisabled]);
 
   let inputMode: any =
     "none" ||
@@ -49,24 +114,12 @@ function TextInput(props: TextInputProps<Inputs>) {
     "search" ||
     undefined;
 
-  let className = {
-    wrapper: {
-      default:
-        "w-full border-b-2 border-b-stone-600 flex flex-row gap-1 pb-1 items-center focus-within:border-blue-600 transition-all",
-      error: "",
-    },
-    input: "outline-none bg-transparent flex-grow",
-    errorText: "text-red-600 text-xs font-semibold",
-  };
-
   if (fieldState && fieldState.error && fieldState.error.message) {
-    const classNameCopy = JSON.parse(JSON.stringify(className));
-    classNameCopy.wrapper.error = "!border-b-red-600";
-    className = { ...classNameCopy };
+    ref.current.wrapper.error = "!border-b-red-600";
+    ref.current.label.error = "text-red-600";
   } else {
-    const classNameCopy = JSON.parse(JSON.stringify(className));
-    classNameCopy.wrapper.error = "";
-    className = { ...classNameCopy };
+    ref.current.wrapper.error = "";
+    ref.current.label.error = "";
   }
 
   if (type === "num") {
@@ -76,16 +129,28 @@ function TextInput(props: TextInputProps<Inputs>) {
   }
 
   return (
-    <div className={`${className.wrapper.default} ${className.wrapper.error}`}>
+    <div
+      className={`${ref.current.wrapper.default} ${ref.current.wrapper.error} ${ref.current.wrapper.disabled}`}
+    >
+      {label && (
+        <label
+          htmlFor={field.name}
+          className={`${ref.current.label.default} ${ref.current.label.error}`}
+        >
+          {label}
+        </label>
+      )}
       <input
         {...field}
+        id={field.name}
         value={field.value ? field.value : ""}
-        className={className.input}
+        className={ref.current.input}
         type={type}
         inputMode={inputMode || "text"}
         placeholder={placeHolder}
+        disabled={isDisabled}
       />
-      <p className={className.errorText}>{fieldState.error?.message}</p>
+      <p className={ref.current.errorText}>{fieldState.error?.message}</p>
       {/* <button className={`${className.btn.btn} ${className.btn.save}`}>
         Save
       </button>
@@ -100,6 +165,104 @@ function TextInput(props: TextInputProps<Inputs>) {
   );
 }
 
+function SelectInput(props: SelectInputs<Inputs>) {
+  const {
+    useControllerProps,
+    placeHolder = "",
+    label = "",
+    selectionItems,
+  } = props;
+  const { field, fieldState } = useController(useControllerProps);
+
+  let className = {
+    wrapper: {
+      default:
+        "w-full border-b border-b-stone-600 flex flex-row items-center focus-within:border-blue-600 transition-all",
+      error: "",
+    },
+    label: {
+      default: "mr-1 font-semibold",
+      error: "",
+    },
+    input: "outline-none bg-transparent flex-grow appearance-none select-input",
+    errorText: "text-red-600 text-xs font-semibold",
+  };
+
+  if (fieldState && fieldState.error && fieldState.error.message) {
+    const classNameCopy = JSON.parse(JSON.stringify(className));
+    classNameCopy.wrapper.error = "!border-b-red-600";
+    classNameCopy.label.error = "text-red-600";
+    className = { ...classNameCopy };
+  } else {
+    const classNameCopy = JSON.parse(JSON.stringify(className));
+    classNameCopy.wrapper.error = "";
+    classNameCopy.label.error = "";
+    className = { ...classNameCopy };
+  }
+
+  return (
+    <div className={`${className.wrapper.default} ${className.wrapper.error}`}>
+      <label
+        htmlFor={field.name}
+        className={`${className.label.default} ${className.label.error}`}
+      >
+        {label}
+      </label>
+      <select
+        {...field}
+        name="cars"
+        id="cars"
+        value={field.value ? field.value : ""}
+        className={className.input}
+      >
+        <option value="">{placeHolder}</option>
+        {selectionItems.map((item, i) => {
+          return (
+            <option key={i} value={item.value}>
+              {item.text}
+            </option>
+          );
+        })}
+      </select>
+      <p className={className.errorText}>{fieldState.error?.message}</p>
+    </div>
+  );
+}
+
+function CheckBox(props: CheckBoxInputs<Inputs>) {
+  const { useControllerProps, label = "" } = props;
+  const { field, fieldState } = useController(useControllerProps);
+
+  let className = {
+    wrapper: {
+      default: "w-full flex flex-row items-center",
+      error: "",
+    },
+    label: {
+      default: "mr-1 font-semibold",
+      error: "",
+    },
+    input: "outline-none bg-transparent flex-grow appearance-none select-input",
+    errorText: "text-red-600 text-xs font-semibold",
+  };
+
+  return (
+    <div className={`${className.wrapper.default} ${className.wrapper.error}`}>
+      <label
+        htmlFor={field.name}
+        className={`${className.label.default} ${className.label.error}`}
+      >
+        {label}
+      </label>
+      <input
+        {...field}
+        value={field.value ? field.value : ""}
+        type="checkbox"
+      />
+    </div>
+  );
+}
+
 export default function Page() {
   const {
     handleSubmit,
@@ -110,36 +273,81 @@ export default function Page() {
     defaultValues: {
       firstName: "",
       age: null,
+      gender: "",
+      isActive: 0,
     },
     mode: "all",
     resolver: yupResolver<Inputs>(schema),
     reValidateMode: "onBlur",
   });
 
-  const onSubmit = (data: Inputs) => console.log(data);
+  const onSubmit: SubmitHandler<Inputs> = (data) => console.log(data);
   console.log("🚀 ~ file: page.tsx:74 ~ Page ~ errors:", errors);
 
   //   console.log(watch("firstName"));
   //   const watchAllFields = watch();
   //   console.log(watchAllFields);
 
+  const selectionItems = [
+    {
+      id: 0,
+      value: "volvo",
+      text: "Volvo",
+    },
+    {
+      id: 1,
+      value: "saab",
+      text: "Saab",
+    },
+    {
+      id: 2,
+      value: "mercedes",
+      text: "Mercedes",
+    },
+    {
+      id: 3,
+      value: "audi",
+      text: "Audi",
+    },
+  ];
+
   return (
     <>
+      {/* <input id="s1" type="checkbox" className="switch" />
+      <input id="s1" type="checkbox" />
+      <input id="s1" type="radio" /> */}
+
       <form
         onSubmit={handleSubmit(onSubmit)}
         className="flex flex-col items-start gap-5"
       >
         <TextInput
+          label="Name"
           type="text"
           placeHolder="Item"
           useControllerProps={{ control, name: "firstName" }}
+          isDisabled={true}
         />
         <TextInput
+          label="Age"
           type="num"
           placeHolder="Item"
           useControllerProps={{ control, name: "age" }}
+          isDisabled={true}
         />
-        <button type="submit">Save 2</button>
+
+        <SelectInput
+          label="Gender"
+          selectionItems={selectionItems}
+          placeHolder="Select One"
+          useControllerProps={{ control, name: "gender" }}
+        />
+
+        <CheckBox
+          label="Is Active"
+          useControllerProps={{ control, name: "isActive" }}
+        />
+        <button type="submit">Save</button>
       </form>
     </>
   );
